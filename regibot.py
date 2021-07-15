929be6934c0592bcff8b2bf675805c6e7e15e72e
bernarducs <bernarducs@gmail.com>
2021-07-14 23:32:07 -0300

    refactoring lyrics.py

--- regibot.py
+++ regibot.py
@@ -6,7 +6,7 @@ import tweepy
 
 from twtr_api import tt_api
 
-from controllers.lyrics import current_song_and_verse
+from controllers.lyrics import song_and_verse
 from controllers.spotify import spotify_link
 from controllers.albums import album_of_day
 from utils.utils import get_emoji, get_delay, get_now, CHIFRE
@@ -16,7 +16,7 @@ api = tt_api.twitter_conexao()
 while True:
     print(get_now())
 
-    verse, song, new_song, last_verse = current_song_and_verse()
+    verse, song, new_song, last_verse = song_and_verse()
     if new_song:
         intro_txt = f"Hoje irei tocar {song} \nSe liga aí, bicho! {CHIFRE}"
         print(intro_txt, '\n')
